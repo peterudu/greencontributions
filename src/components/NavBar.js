e10dc41bc6e90ca44dec3bd0645a7bb8154eff6f
peterudu <puduevborho7@gmail.com>
2024-02-27 04:36:57 +0000

    Add Bootstrap Navbar basic structure, favicon and logo

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap"; 
+import logo from "../assets/logo.png"
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i class="fa-solid fa-house-chimney"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i class="fa-solid fa-right-to-bracket"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+            <i class="fa-solid fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
